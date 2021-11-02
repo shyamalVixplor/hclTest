3d6ff143d9edce29dd82c863a556d93be60f7652
Shyamal Gayen <Shyamal.gayen@yum.com>
2021-11-02 17:45:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = React.useState(0);
+  React.useEffect(() => {
+    if (number === 10) {
+      setNumber(0);
+    }
+  }, [number]);
+  const increaseData = () => {
+    setNumber(number + 1);
+  };
+  const decreaseData = () => {
+    setNumber(number - 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello CodeSandbox</h1>
+    <h2>Start editing to see some magic happen!</h2>
+    <button onClick={increaseData}>Increase</button>
+    <h1>{number}</h1>
+    <button onClick={decreaseData}>Decrease</button>
+  </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+});
+
+describe('Increasse', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
+
+describe('Decrease', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(3 - 2).toBe(1);
+  });
 });
